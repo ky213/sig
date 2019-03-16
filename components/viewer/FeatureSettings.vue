c4882323a9b8bf4609286a5f8842a054654e1e6f
yaaqoub.keddaoui <yaaqoub.keddaoui@eadn.dz>
2019-03-16 18:58:15 +0100

    added type selector and attributes tabs

--- components/viewer/FeatureSettings.vue
+++ components/viewer/FeatureSettings.vue
@@ -12,7 +12,12 @@
           <b-icon pack="fas" icon="image"></b-icon>
           <span>Type</span>
         </template>
-        <b-collapse class="card d-block" aria-id="contentIdForA11y3">
+        <b-collapse
+          v-for="schema in schemas"
+          :key="schema.name"
+          class="card d-block"
+          aria-id="contentIdForA11y3"
+        >
           <div
             slot="trigger"
             slot-scope="props"
@@ -20,75 +25,19 @@
             role="button"
             aria-controls="contentIdForA11y3"
           >
-            <p class="card-header-title">Algérie Télécom</p>
+            <p class="card-header-title">{{schema.name}}</p>
             <a class="card-header-icon">
               <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
             </a>
           </div>
           <div class="card-content">
             <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-              <a>#buefy</a>.
-            </div>
-          </div>
-        </b-collapse>
-        <b-collapse class="card d-block" aria-id="contentIdForA11y3">
-          <div
-            slot="trigger"
-            slot-scope="props"
-            class="card-header"
-            role="button"
-            aria-controls="contentIdForA11y3"
-          >
-            <p class="card-header-title">Algérie Poste</p>
-            <a class="card-header-icon">
-              <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
-            </a>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-              <a>#buefy</a>.
-            </div>
-          </div>
-        </b-collapse>
-        <b-collapse class="card d-block" aria-id="contentIdForA11y3">
-          <div
-            slot="trigger"
-            slot-scope="props"
-            class="card-header"
-            role="button"
-            aria-controls="contentIdForA11y3"
-          >
-            <p class="card-header-title">Djezzy</p>
-            <a class="card-header-icon">
-              <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
-            </a>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-              <a>#buefy</a>.
-            </div>
-          </div>
-        </b-collapse>
-        <b-collapse class="card d-block" aria-id="contentIdForA11y3">
-          <div
-            slot="trigger"
-            slot-scope="props"
-            class="card-header"
-            role="button"
-            aria-controls="contentIdForA11y3"
-          >
-            <p class="card-header-title">ooredoo</p>
-            <a class="card-header-icon">
-              <b-icon :icon="props.open ? 'menu-down' : 'menu-up'"></b-icon>
-            </a>
-          </div>
-          <div class="card-content">
-            <div class="content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-              <a>#buefy</a>.
+              <img
+                :name="schema.name"
+                :src="`/icons/${schema.name}.png`"
+                class="schema-icon"
+                @click="onTypeSelect"
+              >
             </div>
           </div>
         </b-collapse>
@@ -98,13 +47,16 @@
           <b-icon pack="fas" icon="database"></b-icon>
           <span>Attributes</span>
         </template>
+        <b-field v-for="(prop,i) in activeSchema.properties" :key="i" :label="i">
+          <b-input :name="i" :type="prop"></b-input>
+        </b-field>
       </b-tab-item>
     </b-tabs>
-    <footer class="card-footer">
-      <button typ="submit" href="#" class="button is-info is-large card-footer-item">Save
+    <footer class="card-footer bg-white">
+      <button typ="submit" href="#" class="button is-info is-medium card-footer-item">Save
         <b-icon pack="fas" icon="cloud-upload-alt" class="ml-2" size="is-small"></b-icon>
       </button>
-      <button href="#" class="button is-danger is-outlined is-large card-footer-item">Cancel
+      <button href="#" class="button is-danger is-outlined is-medium card-footer-item">Cancel
         <b-icon pack="fas" icon="times" class="ml-2" size="is-small"></b-icon>
       </button>
     </footer>
@@ -112,25 +64,49 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data() {
     return {
+      selectedType: '',
       activeTab: 0
     }
   },
+  computed: {
+    ...mapState({ schemas: state => state.schemas.schemas }),
+    activeSchema() {
+      return (
+        this.schemas
+          .filter(schema => schema.name === this.selectedType)
+          .pop() || {}
+      )
+    }
+  },
   methods: {
+    onTypeSelect(e) {
+      this.selectedType = e.target.name
+      this.activeTab = 1
+    },
     onSubmit(e) {
       console.log('submitted', e)
     }
-  },
-  mounted() {
-    console.log('FORM!!!!', this.$refs.form)
   }
 }
 </script>
 
-<style>
+<style scoped lang='scss'>
 .card-header {
   height: 50px !important;
+}
+
+.schema-icon {
+  height: 70px;
+  padding: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  &:hover {
+    border: 1px solid #dbdbdb;
+  }
 }
 </style>
