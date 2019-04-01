564d1096a9495ce27c296515dde7a6c51cff7fcc
AMIOUR Billel <billel.amiour@eadn.dz>
2019-04-01 11:24:28 +0000

    Add new file

---
+++ deplo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+a=$(docker inspect -f {{.State.Running}} $client)
+if [ $? -eq 1 ]; then
+  echo ""
+else
+  if [ "$a" == "true" ]; then
+    echo "Stoping and removing App container"
+    docker stop "$client"
+  else
+    echo "Removing App container"
+    docker rm "$client"
+  fi
+fi
+
+echo "Building frontend alpha docker images .... "
+docker build -t ci/frontend:alpha . && docker run --name "$client" -p 10.1.1.24:80:80 -d --rm ci/frontend:alpha
