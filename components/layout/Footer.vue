fd1618735538c50a156b1eb1f26a338f4109d0cb
yaaqoub.keddaoui <yaaqoub.keddaoui@eadn.dz>
2019-04-11 12:04:16 +0100

    changed footer title

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="footer is-paddingless" style="padding-top:10px !important">
     <div class="content has-text-centered">
       <p>
-        <strong>kharitaDZ</strong> by
+        <strong>kharitaDZ </strong> by
         <a href="https://eadn.dz" target="_blank">EADN</a>
       </p>
     </div>
